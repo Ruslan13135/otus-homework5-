d50f1da87701e3d0dd8498d8da8224bc0c9686e8
rus <ruslanio.06@mail.ru>
2022-06-20 13:11:26 +0300

    added homework9

--- src/main/java/ot/homework5plus/rushm/domain/Author.java
+++ src/main/java/ot/homework5plus/rushm/domain/Author.java
@@ -1,37 +1,25 @@
 package ot.homework5plus.rushm.domain;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 import java.util.Objects;
 
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "author")
 public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "name", unique = true)
     private String name;
 
-    public Author(Long id) {
-        this.id = id;
-    }
-
-    public Author(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
     public Author(String name) {
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
         this.name = name;
     }
 
